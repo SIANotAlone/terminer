2c1da2e7f8acbcb604717819997601ff8cd6867f
sianotalone <sianotalone@gmail.com>
2024-11-13 19:12:47 +0100

    i was make refactoring create offering

--- internal/service/offering.go
+++ internal/service/offering.go
@@ -18,7 +18,10 @@ func NewOfferingService(repo repository.Offering) *OfferingService {
 }
 
 func (s *OfferingService) CreateService(offering models.NewService) (uuid.UUID, error) {
-
+	repo, err := s.repo.CreateOffering(offering)
+	if err != nil {
+		fmt.Println(err)
+	}
 	obs := observer.ConcreteObserver{}
 	subject := observer.ConcreteSubject{}
 	subject.Register(&obs)
@@ -26,25 +29,13 @@ func (s *OfferingService) CreateService(offering models.NewService) (uuid.UUID, 
 		offering.Service.Name, offering.Service.Description)
 
 	if offering.Service.Available_for_all == true {
-		users, err := s.repo.GetAllUsersTelegramID()
-		if err != nil {
-			println(err)
-		}
-		for _, user := range users {
-			subject.Notify(user, message)
-		}
-		return s.repo.CreateOffering(offering)
+		s.notificateAllUsers(&subject, message)
+		return repo, nil
+	} else {
+		s.notificate_available_for_users(&subject, message, offering.Available_for)
 	}
 
-	for _, user := range offering.Available_for {
-		tg_id, err := s.repo.GetUserTelegramID(user.UserID)
-		if err != nil {
-			println(err)
-		}
-		subject.Notify(tg_id, message)
-		return s.repo.CreateOffering(offering)
-	}
-	return s.repo.CreateOffering(offering)
+	return repo, nil
 }
 
 func (s *OfferingService) UpdateService(service models.ServiceUpdate) error {
@@ -85,4 +76,26 @@ func (s *OfferingService) GetUserTelegramID(user_id uuid.UUID) (string, error) {
 
 func (s *OfferingService) GetAllUsersTelegramID() ([]string, error) {
 	return s.repo.GetAllUsersTelegramID()
+}
+
+func (s *OfferingService) notificateAllUsers(subject *observer.ConcreteSubject, message string) {
+	users, err := s.repo.GetAllUsersTelegramID()
+	if err != nil {
+		println(err)
+	}
+	for _, user := range users {
+		subject.Notify(user, message)
+	}
+}
+
+func (s *OfferingService) notificate_available_for_users(subject *observer.ConcreteSubject, message string, users []models.Available_for) {
+	for _, user := range users {
+		tg_id, err := s.repo.GetUserTelegramID(user.UserID)
+		if err != nil {
+			println(err)
+		}
+		subject.Notify(tg_id, message)
+
+	}
+
 }
